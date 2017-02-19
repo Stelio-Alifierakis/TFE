fcda36aef994bf9dc1a0b7147cb05bd4ea0306c7
Stel-Admin <Stel-Admin>
2017-02-19 11:54:11 +0100

    prepa proxy

---
+++ proxy/IEproxy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proxy
+{
+    public interface IEproxy
+    {
+        void StartProxy();
+        void Stop();
+    }
+}
