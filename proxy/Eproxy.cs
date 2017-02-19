173c355638d622bb3ae3ae8f3ca4787e31a5a0de
Stel-Admin <Stel-Admin>
2017-02-19 15:55:18 +0100

    ajout proxy

--- proxy/Eproxy.cs
+++ proxy/Eproxy.cs
@@ -1,13 +1,155 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Titanium.Web.Proxy;
+using Titanium.Web.Proxy.EventArguments;
+using Titanium.Web.Proxy.Models;
 
 namespace proxy
 {
-    public class Eproxy
+    public class Eproxy : IEproxy
     {
+        private ProxyServer proxyServer;
+        private Dictionary<Guid, string> requestBodyHistory;
 
+        public Eproxy()
+        {
+            proxyServer = new ProxyServer();
+            proxyServer.TrustRootCertificate = true;
+            requestBodyHistory = new Dictionary<Guid, string>();
+        }
+
+        public void StartProxy()
+        {
+            proxyServer.BeforeRequest += OnRequest;
+            proxyServer.BeforeResponse += OnResponse;
+            proxyServer.ServerCertificateValidationCallback += OnCertificateValidation;
+            proxyServer.ClientCertificateSelectionCallback += OnCertificateSelection;
+
+            var explicitEndPoint = new ExplicitProxyEndPoint(IPAddress.Any, 8000, true)
+            {
+                // ExcludedHttpsHostNameRegex = new List<string>() { "google.com", "dropbox.com" }
+            };
+
+            proxyServer.AddEndPoint(explicitEndPoint);
+            proxyServer.Start();
+
+            var transparentEndPoint = new TransparentProxyEndPoint(IPAddress.Any, 8001, true)
+            {
+                GenericCertificateName = "google.com"
+            };
+            proxyServer.AddEndPoint(transparentEndPoint);
+
+            foreach (var endPoint in proxyServer.ProxyEndPoints)
+                Console.WriteLine("Listening on '{0}' endpoint at Ip {1} and port: {2} ",
+                    endPoint.GetType().Name, endPoint.IpAddress, endPoint.Port);
+
+            proxyServer.SetAsSystemHttpProxy(explicitEndPoint);
+            proxyServer.SetAsSystemHttpsProxy(explicitEndPoint);
+        }
+
+        public void Stop()
+        {
+            proxyServer.BeforeRequest -= OnRequest;
+            proxyServer.BeforeResponse -= OnResponse;
+            proxyServer.ServerCertificateValidationCallback -= OnCertificateValidation;
+            proxyServer.ClientCertificateSelectionCallback -= OnCertificateSelection;
+
+            proxyServer.Stop();
+        }
+
+        public async Task OnRequest(object sender, SessionEventArgs e)
+        {
+            //Console.WriteLine(e.WebSession.Request.Url);
+
+            var requestHeaders = e.WebSession.Request.RequestHeaders;
+
+            var method = e.WebSession.Request.Method.ToUpper();
+            if ((method == "POST" || method == "PUT" || method == "PATCH"))
+            {
+                //Get/Set request body bytes
+                byte[] bodyBytes = await e.GetRequestBody();
+                await e.SetRequestBody(bodyBytes);
+
+                //Get/Set request body as string
+                string bodyString = await e.GetRequestBodyAsString();
+                await e.SetRequestBodyString(bodyString);
+
+                requestBodyHistory[e.Id] = bodyString;
+            }
+
+
+            //To cancel a request with a custom HTML content
+            //Filter URL
+            if (e.WebSession.Request.RequestUri.AbsoluteUri.Contains("google.com"))
+            {
+                await e.Ok("<!DOCTYPE html>" +
+                      "<html><body><h1>" +
+                      "Website Blocked" +
+                      "</h1>" +
+                      "<p>Blocked by titanium web proxy.</p>" +
+                      "</body>" +
+                      "</html>");
+            }
+            //Redirect example
+            if (e.WebSession.Request.RequestUri.AbsoluteUri.Contains("wikipedia.org"))
+            {
+                await e.Redirect("https://www.paypal.com");
+            }
+
+        }
+
+        public async Task OnResponse(object sender, SessionEventArgs e)
+        {
+            if (requestBodyHistory.ContainsKey(e.Id))
+            {
+                //access request body by looking up the shared dictionary using requestId
+                var requestBody = requestBodyHistory[e.Id];
+            }
+
+            //read response headers
+            var responseHeaders = e.WebSession.Response.ResponseHeaders;
+
+            // print out process id of current session
+            //Console.WriteLine($"PID: {e.WebSession.ProcessId.Value}");
+
+            //if (!e.ProxySession.Request.Host.Equals("medeczane.sgk.gov.tr")) return;
+            if (e.WebSession.Request.Method == "GET" || e.WebSession.Request.Method == "POST")
+            {
+                if (e.WebSession.Response.ResponseStatusCode == "200")
+                {
+                    if (e.WebSession.Response.ContentType != null && e.WebSession.Response.ContentType.Trim().ToLower().Contains("text/html"))
+                    {
+                        byte[] bodyBytes = await e.GetResponseBody();
+                        await e.SetResponseBody(bodyBytes);
+
+                        string body = await e.GetResponseBodyAsString();
+                        Console.WriteLine("---------------------------->" + body);
+                        await e.SetResponseBodyString(body);
+                    }
+                }
+            }
+        }
+
+        public Task OnCertificateValidation(object sender, CertificateValidationEventArgs e)
+        {
+            //set IsValid to true/false based on Certificate Errors
+            if (e.SslPolicyErrors == System.Net.Security.SslPolicyErrors.None)
+            {
+                e.IsValid = true;
+            }
+
+            return Task.FromResult(0);
+        }
+
+        public Task OnCertificateSelection(object sender, CertificateSelectionEventArgs e)
+        {
+            //set e.clientCertificate to override
+
+            return Task.FromResult(0);
+        }
     }
 }
--- vueConsole/Program.cs
+++ vueConsole/Program.cs
@@ -5,13 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using ProxyNavigateur;
 using Rechercheur;
+using proxy;
+using System.Runtime.InteropServices;
 
 namespace vueConsole
 {
     class Program
     {
+        private static readonly IEproxy Controller = new Eproxy();
+
         static void Main(string[] args)
         {
+            NativeMethods.Handler = ConsoleEventCallback;
+            NativeMethods.SetConsoleCtrlHandler(NativeMethods.Handler, true);
+
             Console.WriteLine(System.IO.Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]));
             DAL d = new DAL(System.IO.Path.GetDirectoryName(Environment.GetCommandLineArgs()[0])+ "/test.sqlite");
             Rechercheur.Rechercheur r = new Rechercheur.Rechercheur(d);
@@ -32,7 +39,41 @@ namespace vueConsole
             Console.WriteLine(r.checkPartWord("partout").ToString());
             Console.WriteLine(r.valPhrase("pas de sex, pas de sex, pas de sex, pas de sex, pas de sex, pas de seks <br> du porn, encore du porn et du sexe, pas de sex, pas de seks <br> du porn, encore du porn et du sexe, pas de sex, pas de seks <br> du porn, encore du porn et du sexe, pas de sex, pas de seks <br> du porn, encore du porn et du sexe, pas de sex, pas de seks <br> du porn, encore du porn et du sexe,pas de sex, pas de seks <br> du porn, encore du porn et du sexe").ToString());
 
-            bool essai = (Console.ReadKey().Key == ConsoleKey.O);
+            Controller.StartProxy();
+
+            Console.WriteLine("Hit any key to exit..");
+            Console.WriteLine();
+            Console.Read();
+
+            Controller.Stop();
+
+            //bool essai = (Console.ReadKey().Key == ConsoleKey.O);
         }
+
+        private static bool ConsoleEventCallback(int eventType)
+        {
+            if (eventType != 2) return false;
+            try
+            {
+                Controller.Stop();
+            }
+            catch
+            {
+                // ignored
+            }
+            return false;
+        }
+    }
+
+    internal static class NativeMethods
+    {
+        // Keeps it from getting garbage collected
+        internal static ConsoleEventDelegate Handler;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool SetConsoleCtrlHandler(ConsoleEventDelegate callback, bool add);
+
+        // Pinvoke
+        internal delegate bool ConsoleEventDelegate(int eventType);
     }
 }
