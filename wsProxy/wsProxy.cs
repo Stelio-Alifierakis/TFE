985d51cdbc89c07f0ac873264da6ac97d3c5ed3b
Stel-Admin <Stel-Admin>
2017-03-05 15:47:04 +0100

    ajout service et installation

---
+++ wsProxy/wsProxy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wsProxy
+{
+    public partial class wsProxy : ServiceBase
+    {
+        public wsProxy()
+        {
+            InitializeComponent();
+            if (!System.Diagnostics.EventLog.SourceExists("testLog"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource("testLog","newLog");
+            }
+            eventLog1.Source = "testLog";
+            eventLog1.Log = "newLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("est démarré");
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("est éteint");
+        }
+    }
+}
